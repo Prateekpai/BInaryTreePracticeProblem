e870d7022614e7253eb3f2abe020f84396533e9d
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-27 10:21:01 +0530

    Ability to Create Binary Tree as per the Figure

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BinaryTreeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Add(56);
+            binaryTree.Add(70);
+            binaryTree.Add(95);
+            binaryTree.Add(60);
+            binaryTree.Add(65);
+            binaryTree.Add(67);
+            binaryTree.Add(63);
+            binaryTree.Add(30);
+            binaryTree.Add(40);
+            binaryTree.Add(22);
+            binaryTree.Add(11);
+            binaryTree.Add(16);
+            binaryTree.Add(3);
+            binaryTree.TravarsePreorder(binaryTree.Root);
+
+        }
+    }
+}
