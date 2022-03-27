abdbfe4a691e017d13d2b0ca26c8df6d8b5aaba6
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-27 10:19:32 +0530

    Ability to Create Binary Tree

---
+++ Node.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BinaryTreeDemo
+{
+    public class Node
+    {
+        public Node LeftNode { get; set; }
+        public Node RightNode { get; set; }
+        public int Data { get; set; }
+    }
+}
