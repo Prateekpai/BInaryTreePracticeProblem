442f9391c5dd069a216c416b9e736ce07821bfcb
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-27 10:15:58 +0530

    Welcome to BinaryTree Practice Problem

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BinaryTreeDemo
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcometo Binary Tree Problem");
+        }
+    }
+}
